286ee0690b213558df6b4b3233b92f9108b56792
bombenten <50871432+bombenten@users.noreply.github.com>
2021-08-24 15:51:00 +0700

    fix v.2

--- WorkShop/BMI.js
+++ WorkShop/BMI.js
@@ -1,19 +1,17 @@
 
-//input ค่าเอง
-let pound = [36.5, 35.5, 34.5];
-let inch = [39, 38, 37];
+//input ให้ pound & inch มีจำนวนข้อมูลเท่ากัน
+let pound = [36.5, 35.5, 34.5, 33.5];
+let inch = [39, 38, 37, 36];
 
 const convertKg = 0.453592;
 const convertM = 0.0254;
-
-let newLine = "\r\n";
 
 function bmi(pound, inch) {
     let kilogram;
     let meter;
     let bmi = [];
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < pound.length; i++) {
         let pound2 = pound[i];
         let inch2 = inch[i];
 
@@ -24,7 +22,8 @@ function bmi(pound, inch) {
     } return bmi;
 
 }
-for (let j = 0; j < 3; j++) {
+
+for (let j = 0; j < pound.length; j++) {
     console.log('Pound : ' + pound[j] + ' | Inch : ' + inch[j]);
     console.log('Your BMI is : ' + bmi(pound, inch)[j]);
 }
