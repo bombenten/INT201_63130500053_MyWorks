8d9a448c5a1b1034335e23bdf3f7636d9bdf526d
bombenten <bombenten@hotmail.com>
2021-09-21 10:55:34 +0700

    update file

--- 04_Objects/script.js
+++ 04_Objects/script.js
@@ -1,85 +1,37 @@
-// let person = {}; //let person = new Object();
-// console.log(typeof person);
-// person.id = 12345678;
-// person.name = 'Somchai';
-// console.log(person);
-// person.email = 'somchai@gmail.com';
-// console.log(person);
-// delete person.email;
-// console.log(person);
+let player1 = { id: 1, name: 'Adam' }; //let player1=new Object()
+console.log(Object.prototype.isPrototypeOf(player1));
 
-//---------------------------------------------------------
+let adminPlayer = Object.create(player1);
+adminPlayer.level = 'super';
 
-// class Person {
-//     constructor(id, name) {
-//         if (id.length != 13) this._id = 'unknown';
-//         else this._id = id;
-//         this._name = name;
-//     }
-//     //getter
-//     get id() {
-//         return this._id;
-//     }
-//     //setter
-//     set id(id) {
-//         this._id = id; //backing field
-//     }
-//     // //method
-//     getPerson() {
-//         return `person id = ${this._id}, person name = ${this._name}`;
-//     }
-// }
+console.log(`${adminPlayer.id}, ${adminPlayer.name}, ${adminPlayer.level}`);
 
-// let per1 = new Person('1234567', 'Somsak');
-// // console.log(per1.toString());
-
-// let per2 = new Person('1234567891012', 'Somsri');
-// // console.log(per2.toString());
-
-// console.log(per1);
-// console.log(per2);
-// console.log(per1.getPerson());
-
-//---------------------------------------------------------
-
-class Person {
-    constructor(id, name) {
-        if (id.length != 13) this._id = 'unknown';
-        else this._id = id;
-        this._name = name;
-    }
-    //getter
-    get id() {
-        return this._id;
-    }
-    //setter
-    set id(id) {
-        this._id = id; //backing field
-    }
-    // //method
-    toString() {
-        return `person id = ${this._id}, person name= ${this._name}`;
-    }
+let playerKeys = Object.keys(player1);
+for (let p of playerKeys) {
+  console.log(`key: ` + p);
 }
+class Dice {
+  constructor(gameTitle) {
+    console.log('constructor is working');
+    this._diceName = gameTitle;
+    this._faceValue = 0;
+  }
+  roll() {
+    this._faceValue = Math.floor(Math.random() * 6) + 1;
+  }
+  get faceValue() {
+    return this._faceValue;
+  }
+}
+//#1
+let dice1 = new Dice();
+let dice2 = new Dice();
 
-let per1 = new Person('1234567', 'SomSri');
-console.log(per1.toString());
-
-let per2 = new Person('1234567891234', 'Somsak');
-console.log(per2.toString());
-
-per2.getFirstNameLetter = function () {
-    return this._name.charAt(0);
+Dice.prototype.toString = function () {
+  return `game = ${this._diceName}, faceValue = ${this._faceValue}`;
 };
-console.log(per2.getFirstNameLetter());
 
-//   console.log(per2.getFirstNameLetter());
-//   console.log(per1.getFirstNameLetter());
-  //per1.getFirstNameLetter is not a function
+console.log(dice1.toString());
+console.log(dice2.toString());
 
-  // Person.prototype.getFirstNameLetter = function () {
-  //   return this._name.charAt(0);
-  // };
-
-  // console.log(per2.getFirstNameLetter());
-  // console.log(per1.getFirstNameLetter());
+console.log(Dice.prototype.isPrototypeOf(player1));
